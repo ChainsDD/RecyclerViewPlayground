91971172ed5b8e6ae5eee3d5633654f6cc3e97a4
Adam Shanks <chainsdd@gmail.com>
2016-04-23 23:25:54 -0500

    Add GroupDelegateAdapter

--- app/src/main/kotlin/com/noshufou/recyclerviewplayground/adapter/DelegateAdapter.kt
+++ app/src/main/kotlin/com/noshufou/recyclerviewplayground/adapter/DelegateAdapter.kt
@@ -4,23 +4,21 @@ import android.support.v4.util.SparseArrayCompat
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.ViewGroup
-import com.noshufou.recyclerviewplayground.R
 import com.noshufou.recyclerviewplayground.common.GroupItem
 import com.noshufou.recyclerviewplayground.common.PersonItem
-import com.noshufou.recyclerviewplayground.common.inflate
-import kotlinx.android.synthetic.main.header_item.view.*
 import java.util.*
 
 /**
  * Created by Adam on 4/22/2016.
  */
 
-class DelegateAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(), StickyHeaderAdapter<DelegateAdapter.HeaderViewHolder> {
+class DelegateAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private var groups: ArrayList<GroupItem>
     private var items: ArrayList<ViewType>
     private var delegateAdapters = SparseArrayCompat<ViewTypeDelegateAdapter>()
 
     init {
+        delegateAdapters.put(AdapterConstants.GROUP, GroupDelegateAdapter())
         delegateAdapters.put(AdapterConstants.PERSON, PersonDelegateAdapter())
         groups = ArrayList()
         items = ArrayList()
@@ -42,19 +40,6 @@ class DelegateAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(), StickyH
         return this.items[position].getViewType()
     }
 
-    override fun getHeaderId(position: Int): Int {
-        return (items[position] as PersonItem).group
-    }
-
-    override fun onCreateHeaderViewHolder(parent: ViewGroup): HeaderViewHolder {
-        return HeaderViewHolder(parent)
-    }
-
-    override fun onBindHeaderViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        holder as HeaderViewHolder
-        holder.bind(groups[(items[position] as PersonItem).group])
-    }
-
     fun addPeople(people: List<PersonItem>) {
         val positionStart = items.size
         items.addAll(people)
@@ -64,13 +49,6 @@ class DelegateAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(), StickyH
 
     fun addGroups(groups: List<GroupItem>) {
         this.groups.addAll(groups)
-    }
-
-    class HeaderViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
-            parent.inflate(R.layout.header_item)) {
-        fun bind(item: GroupItem) = with(itemView) {
-            name.text = item.name
-        }
     }
 
 }
---
+++ app/src/main/kotlin/com/noshufou/recyclerviewplayground/adapter/GroupDelegateAdapter.kt
@@ -0,0 +1,30 @@
+package com.noshufou.recyclerviewplayground.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.noshufou.recyclerviewplayground.R
+import com.noshufou.recyclerviewplayground.common.GroupItem
+import com.noshufou.recyclerviewplayground.common.inflate
+import kotlinx.android.synthetic.main.item_group.view.*
+
+/**
+ * Created by Adam on 4/23/2016.
+ */
+
+class GroupDelegateAdapter : ViewTypeDelegateAdapter {
+    override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+        throw UnsupportedOperationException()
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: ViewType) {
+        throw UnsupportedOperationException()
+    }
+
+    class GroupViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+            parent.inflate(R.layout.item_group)
+    ) {
+        fun bind(item: GroupItem) = with(itemView) {
+            name.text = item.name
+        }
+    }
+}
