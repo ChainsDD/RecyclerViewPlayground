1bce2464050f31ae19f554c15b3e8d4f6d05310b
Adam Shanks <chainsdd@gmail.com>
2016-04-25 18:15:55 -0500

    Consolidate extension functions

--- app/src/main/kotlin/com/noshufou/recyclerviewplayground/common/Extensions.kt
+++ app/src/main/kotlin/com/noshufou/recyclerviewplayground/common/Extensions.kt
@@ -9,10 +9,7 @@ import android.view.ViewGroup
  * Created by Adam on 4/22/2016.
  */
 
-fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
-    return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
-}
+fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false)
+        = LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
 
-fun RecyclerView.getViewType(view: View): Int {
-    return adapter.getItemViewType(getChildAdapterPosition(view))
-}
+fun RecyclerView.getViewType(view: View) = adapter.getItemViewType(getChildAdapterPosition(view))
